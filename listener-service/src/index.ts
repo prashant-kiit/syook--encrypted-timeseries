74532ab14211235b2aa2257f333df124672da490
Prashant Singh <prashant.singh@konovo.com>
2026-02-19 07:37:20 +0530

    chore: initated listerner service

---
+++ listener-service/src/index.ts
@@ -0,0 +1,37 @@
+// listener/src/index.ts
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import "dotenv/config";
+
+const app = express();
+const httpServer = createServer(app);
+
+const LISTENER_PORT = process.env.LISTENER_PORT;
+if (!LISTENER_PORT) {
+  throw new Error("LISTENER_PORT environment variable is not defined");
+}
+
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
+
+app.get("/", (_, res) => {
+  res.send("Listener service running");
+});
+
+io.on("connection", (socket) => {
+  console.log("Emitter connected:", socket.id);
+
+  socket.on("data-stream", (payload: string) => {
+    console.log("Received stream:", payload);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Emitter disconnected");
+  });
+});
+
+httpServer.listen(LISTENER_PORT, () => {
+  console.log(`Listener running on port ${LISTENER_PORT}`);
+});
