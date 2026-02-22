7c50314ceb86f10004487e860e2ce2747589d548
prashant-m4 <prashantsingh090798@gmail.com>
2026-02-22 14:43:41 +0530

    chore: dockerized the frontend app

--- dashboard-frontend/src/App.tsx
+++ dashboard-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 type SuccessGrid = {
   _id: string;
